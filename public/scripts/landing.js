642b1c29fae137d8d2820a7ead0d7c1aa68eb4d4
vasiliosnicholas <vasiliosnicholasgr@gmail.com>
2026-06-16 21:44:33 -0400

    renamed variable

--- public/scripts/landing.js
+++ public/scripts/landing.js
@@ -15,11 +15,11 @@ const categories = {
   ],
 }; //TODO: Replace this with data from Databases.
 
-let searchParam = `Services`;
+let searchType = `Services`;
 
 function populateDataList() {
   dataList.innerHTML = ``;
-  for (const option of categories[searchParam]) {
+  for (const option of categories[searchType]) {
     const optn = document.createElement("option");
     optn.value = option;
     dataList.appendChild(optn);
@@ -32,7 +32,7 @@ function genDropDown() {
     const btn = document.createElement("button");
     btn.classList.add("dropdown-item");
     btn.innerHTML = `by ${category}`;
-    if (category === searchParam) {
+    if (category === searchType) {
       btn.classList.add("active");
     }
     dropDown.appendChild(btn);
@@ -45,7 +45,7 @@ function genDropDown() {
           }
         }
         btn.classList.add("active");
-        searchParam = btn.innerHTML.replace("by ", "");
+        searchType = btn.innerHTML.replace("by ", "");
         dropDownBtnName.innerHTML = btn.innerHTML;
         populateDataList();
       }
@@ -57,5 +57,5 @@ populateDataList();
 
 searchForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  window.location.href = `${SEARCH_URL}?searchType=${searchParam.toLowerCase()}&searchString=${searchBar.value.replace(" ", "_")}`;
+  window.location.href = `${SEARCH_URL}?searchType=${searchType.toLowerCase()}&searchString=${searchBar.value.replace(" ", "_")}`;
 });
