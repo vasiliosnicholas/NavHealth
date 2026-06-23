15c446182c5644a8d44bba91fc618c0f63639add
vasiliosnicholas <vasiliosnicholasgr@gmail.com>
2026-06-23 18:07:13 -0400

    add rating svg

--- public/scripts/reviews.js
+++ public/scripts/reviews.js
@@ -127,7 +127,13 @@ async function deleteReview(review_id) {
 
 function genReviews() {
   reviewsDocument.average_rating = parseFloat(reviewsDocument.average_rating);
-  ratingElement.innerHTML = `Overall Rating: ${reviewsDocument.num_reviews > 0 ? reviewsDocument.average_rating.toFixed(FLOAT_PRECISION) : "Unrated"}`;
+  ratingElement.innerHTML = `Overall Rating: ${
+    reviewsDocument.num_reviews > 0
+      ? `${reviewsDocument.average_rating.toFixed(FLOAT_PRECISION)} <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 1 18 18">
+  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"></path>
+</svg> `
+      : "Unrated"
+  }`;
   for (const description of descriptions) {
     description.classList.remove("placeholder");
     description.innerHTML = `Reviews for ${reviewsDocument.business_name}`;
@@ -176,8 +182,10 @@ function genReviews() {
     reviewHeader.appendChild(secondRow);
 
     const reviewRating = document.createElement("small");
-    reviewRating.className = `mb-1 col-8 `;
-    reviewRating.innerHTML = `Rating: ${review.rating}`;
+    reviewRating.className = `mb-1 col-8`;
+    reviewRating.innerHTML = `Rating: ${review.rating} <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 1 18 18">
+  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"></path>
+</svg>`;
     secondRow.appendChild(reviewRating);
 
     const userName = document.createElement("small");
@@ -297,7 +305,13 @@ function genReviews() {
           "num_reviews",
           -1,
         );
-        ratingElement.innerHTML = `Overall Rating: ${reviewsDocument.num_reviews > 0 ? reviewsDocument.average_rating.toFixed(FLOAT_PRECISION) : "unrated"}`;
+        ratingElement.innerHTML = `Overall Rating: ${
+          reviewsDocument.num_reviews > 0
+            ? `${reviewsDocument.average_rating.toFixed(FLOAT_PRECISION)} <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 1 18 18">
+  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"></path>
+</svg>`
+            : "unrated"
+        }`;
         console.log(reviewsDocument.num_reviews);
         if (reviewsDocument.num_reviews <= 0) {
           reviewsParentElement.innerHTML = "All reviews deleted!";
