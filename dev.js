03d6d51f056c9b16e2ba1f1150415b88b902dda7
vasiliosnicholas <vasiliosnicholasgr@gmail.com>
2026-06-23 20:25:05 -0400

    add comments

--- dev.js
+++ dev.js
@@ -2,6 +2,10 @@ import app from "./app.js";
 import liveReload from "livereload";
 import connectLiveReload from "connect-livereload";
 
+/**
+ * Code for developers to run our express server
+ * within a live-reload server that refreshes the browser
+ */
 const liveServer = liveReload.createServer();
 liveServer.watch("*/*");
 liveServer.server.once("connection", () => {
